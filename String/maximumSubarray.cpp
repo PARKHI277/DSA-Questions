83ef0fcf3fde03a0e2a1e2d61040d04572cc360d
Parkhi <parkhigarg277@gmail.com>
2023-01-31 23:28:00 +0530

    subarray

---
+++ String/maximumSubarray.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) 
+    {
+        int sum = 0;
+        int max_sum = INT_MIN;
+        
+        for(int i=0; i<nums.size();i++)
+        {
+            sum += nums[i];
+            
+            if(max_sum<sum)
+                max_sum = sum;
+            
+            if(sum<0)
+                sum=0;
+        }
+        
+        return max_sum;
+    }
+    
+    
+    
+};
