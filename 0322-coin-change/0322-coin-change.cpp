f2331ca33e2797d9d13f2393314bf50bec0a0bb6
Parkhi Garg <76432727+PARKHI277@users.noreply.github.com>
2023-02-28 23:10:07 +0530

    Time: 71 ms (90.21%), Space: 14.6 MB (62.94%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) 
+    {
+        int n=coins.size();
+        vector <int> dp (amount+1, amount+1);
+        dp[0]=0;
+        
+        for(int i=1; i<dp.size(); i++)
+            for(auto coin : coins)
+                if(coin<=i)
+                    dp[i]=min(dp[i], 1+dp[i-coin]);
+            
+        return dp[amount] == amount+1? -1 : dp[amount];
+    }
+};
+
