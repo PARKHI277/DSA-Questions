f32e4d7db4ca8cf2b9448dd810982d66f0664c9d
Parkhi Garg <76432727+PARKHI277@users.noreply.github.com>
2023-06-01 23:02:00 +0530

    Time: 20 ms (60.19%), Space: 13 MB (42.21%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.cpp
+++ 1035-uncrossed-lines/1035-uncrossed-lines.cpp
@@ -1,32 +1,7 @@
 class Solution {
 public:
-    
-//     int find(int i , int j  , vector<int>&nums1,vector<int>&nums2,vector<vector<int>>&dp)
-//     {
-//         if(i == nums1.size() && j == nums2.size())
-//         {
-//             return 0;
-//         }
-        
-//         if(dp[i][j] != -1)
-//         {
-//             return dp[i][j];
-//         }
-        
-//         int count = 0;
-//         if(nums1[i] == nums2[j])
-//         {
-//             count = 1 + find(i+1,j+1,nums1,nums2,dp);
-//         }
-//         else
-//         {
-//             count += max(find(i+1,j,nums1,nums2,dp),find(i,j+1,nums1,nums2,dp));
-//         }
-        
-//         return dp[i][j]  = count;
-//     }
     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size();
+         int n = nums1.size();
         int m = nums2.size();
        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
         for(int i = 1;i<=n;i++)
