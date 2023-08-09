07cbdc09ede8b8326b1c712db8ae235be2287599
Parkhi Garg <76432727+PARKHI277@users.noreply.github.com>
2023-08-09 18:04:38 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Largest prime factor/largest-prime-factor.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public: 
+    long long int largestPrimeFactor(int N){
+        // code here
+        long long ans = 2;
+        while(N>ans)
+        {
+            if(N%ans == 0)
+            {
+                N  = N/ans;
+            }
+            else
+            {
+                ans++;
+            }
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.largestPrimeFactor(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
