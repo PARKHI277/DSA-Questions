d2f87dc46cc5adbbfd9b5abe9aebbc16b851767c
Parkhi Garg <76432727+PARKHI277@users.noreply.github.com>
2023-08-23 15:24:25 +0530

    Time: 15 ms (60.75%), Space: 12 MB (7.27%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,23 +3,21 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int,int>m;
         int n = nums.size();
-        for(int i  = 0;i<n;i++)
+        for(int i = 0;i<n;i++)
         {
             m[nums[i]] = i;
+            
         }
         
         for(int i = 0;i<n;i++)
         {
-            int c  = target-nums[i];
-            if(m.count(c) && m[c] != i)
+            int x = target - nums[i];
+            if(m.count(x) && m[x] != i)
             {
-                return {i,m[c]};
+                return {i,m[x]};
             }
         }
         
         return {};
     }
-    
 };
-
-
