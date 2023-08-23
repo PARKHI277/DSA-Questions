87078ef81230ce7f772bd8c43b45981065546e33
Parkhi Garg <76432727+PARKHI277@users.noreply.github.com>
2023-08-23 15:54:57 +0530

    Time: 2 ms (72.34%), Space: 6 MB (25.09%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n<=1)
+        int f[n+2];
+        int i;
+        f[0] = 0;
+        f[1] = 1;
+        for(int i  = 2;i<=n;i++)
         {
-            return n;
-            
+            f[i] = f[i-1] + f[i-2];
         }
-        return fib(n-1) + fib(n-2);
+        
+        return f[n];
     }
 };
