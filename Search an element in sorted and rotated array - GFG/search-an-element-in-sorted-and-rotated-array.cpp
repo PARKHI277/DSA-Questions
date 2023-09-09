c5d3b66df84f81758b7565e9fd89f4c7bb896b00
Parkhi Garg <76432727+PARKHI277@users.noreply.github.com>
2023-09-10 00:57:00 +0530

    Added solution - LeetHub

---
+++ Search an element in sorted and rotated array - GFG/search-an-element-in-sorted-and-rotated-array.cpp
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int Search(vector<int> ,int );
+
+//User code will be pasted here
+
+int main(){
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+        
+        vector<int> vec(n);
+        
+        for(int i =0;i<n;i++) 
+            cin >> vec[i];
+        
+        int target;
+        cin >> target;
+        
+        cout << Search(vec,target) << "\n";
+         
+    }
+}
+// } Driver Code Ends
+
+
+//User function template for C++
+
+// vec : given vector of elements
+// K : given value whose index w need to find 
+int Search(vector<int> arr, int k) {
+    //code here
+    int low = 0;
+    int high = arr.size() - 1;
+    while(low <= high)
+    {
+        int mid = low + (high-low)/2;
+        if(arr[mid] == k)
+        {
+            return mid;
+        }
+        // check for left sorted
+        else if(arr[low] <= arr[mid])
+        {
+            if(arr[low] <= k && k < arr[mid])
+            {
+                high = mid-1;
+            }
+            else
+            {
+                low = mid+1;
+            }
+        }
+        else
+        {
+            if(arr[mid] < k && k<= arr[high])
+            {
+                low = mid+1;
+            }
+            else
+            {
+               high = mid-1;
+            }
+        }
+    }
+    
+    return -1;
+}
