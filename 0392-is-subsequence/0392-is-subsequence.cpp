c6c307994a7c3a8027e98aa59d23d5bb1b275360
Parkhi Garg <76432727+PARKHI277@users.noreply.github.com>
2023-09-22 15:39:00 +0530

    Time: 20 ms (5.44%), Space: 286.3 MB (5.02%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,26 +1,34 @@
 class Solution {
 public:
-   bool isSubs(string& s, string& t, int m, int n)
+    
+    bool check(string s , string t , int m , int n)
     {
         if(m == 0)
+        {
             return true;
+        }
         if(n == 0)
+        {
             return false;
+        }
         
-        
-        if (s[m - 1] == t[n - 1])
-            return isSubs(s, t, m - 1, n - 1);
- 
-       
-        return isSubs(s, t, m, n - 1);
+        if(s[m-1] == t[n-1])
+        {
+            return check(s,t,m-1,n-1);
+        }
+        else
+        {
+            return  check(s,t,m,n-1);
+        }
     }
-    
-    
     bool isSubsequence(string s, string t) {
-        
-       if( isSubs(s,t,s.length(),t.length()))
-           return true;
-        
-        return false;
+        if(check(s,t,s.length(),t.length()))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 };
