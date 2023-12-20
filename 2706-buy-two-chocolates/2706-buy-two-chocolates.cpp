e20d46d732d7a35c623fdc64d3a5a30b0a65127f
Parkhi Garg <76432727+PARKHI277@users.noreply.github.com>
2023-12-20 19:41:29 +0530

    Time: 21 ms (9.32%), Space: 46.9 MB (58.30%) - LeetHub

---
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int buyChoco(vector<int>& price, int money) {
+        sort(price.begin(),price.end());
+        return (money >= (price[0]+price[1])) ? money - (price[0]+price[1]):money;
+    }
+};
+
